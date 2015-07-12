741c35aef4872cb82abb41fd417bdb724dcd67f0
Priyesh Rughani <priyesh1983@gmail.com>
2015-07-12 20:15:58 +0100

    GC counter, Qscore polter
    	new file:   SRR835775_1.first1000.fastq
    	modified:   readFastq.py

--- readFastq.py
+++ readFastq.py
@@ -24,3 +24,64 @@ def readfastq(filename):
 
 seqnames, seqs, quals = readfastq('SRR835775_1.first1000.fastq')
 
+
+def phred33ToQ(qual):
+    return ord(qual) - 33
+
+
+
+
+def createHist(qualityStrings):
+    # Create a histogram of quality scores
+    hist = [0]*50
+    for read in qualityStrings:
+        for phred in read:
+            q = phred33ToQ(phred)
+            hist[q] += 1
+    return hist
+
+
+def findGCByPos(reads):
+    ''' Find the GC ratio at each position in the read '''
+
+    # Keep track of the number of G/C bases and the total number of bases at each position
+    gc = [0] * 100
+    totals = [0] * 100
+
+    for read in reads:
+        for i in range(len(read)):
+            if read[i] == 'C' or read[i] == 'G':
+                gc[i] += 1
+            totals[i] += 1
+
+    # Divide G/C counts by total counts to get the average at each position
+    for i in range(len(gc)):
+        if totals[i] > 0:
+            gc[i] /= float(totals[i])
+
+    return gc
+
+
+import collections
+count = collections.Counter()
+for seq in seqs:
+    count.update(seq)
+count
+
+
+h = createHist(quals)
+print(h)
+
+
+import matplotlib.pyplot as plt
+plt.plot(range(len(h)), h)
+plt.show()
+
+
+gc = findGCByPos(seqs)
+plt.plot(range(len(gc)), gc)
+plt.show()
+
+
+
+
